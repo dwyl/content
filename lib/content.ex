c15069af9030ca98403a647f76fb90af8e3ac425
nelsonic <nelson+github@dwyl.io>
2020-05-18 22:21:25 +0100

    invoke plugs inside call/2 #1

--- lib/content.ex
+++ lib/content.ex
@@ -4,7 +4,7 @@ defmodule Content do
   Please see: github.com/dwyl/content for detail.
   """
   # https://hexdocs.pm/plug/readme.html#the-plug-conn-struct
-  import Plug.Conn
+  # import Plug.Conn
   # https://hexdocs.pm/logger/Logger.html
   # require Logger
 
@@ -17,14 +17,42 @@ defmodule Content do
   def init(options) do
     # return options unmodified
     options
+    |> IO.inspect(label: "options:20")
   end
 
   @doc """
   `call/2` is invoked to handle each HTTP request which `Content` inspects.
   """
   def call(conn, options) do
+    if get_accept_header(conn) =~ "json" do
+      conn
+    else
+      IO.inspect(options, label: "options")
+      # if accept header not "json" then assume "html"
+      # is_function(self) -> "function"
+      for f <- options.plugs do
+        IO.inspect(f, label: "f")
+        IO.inspect(is_function(f), label: "is_function(f)")
+        # https://stackoverflow.com/questions/22562192/function-as-a-parameter
+        conn = f.(conn, [])
+      end
 
-    conn
+      conn
+    end
+  end
+
+  @doc """
+  `get_accept_header/2` gets the "accept" header from req_headers.
+  Defaults to "text/html" if no header is set.
+  """
+  def get_accept_header(conn) do
+    case List.keyfind(conn.req_headers, "accept", 0) do
+      {"accept", accept} ->
+        accept
+
+      nil ->
+        "text/html"
+    end
   end
 
   def hello do
--- test/content_test.exs
+++ test/content_test.exs
@@ -1,6 +1,30 @@
 defmodule ContentTest do
   use ExUnit.Case
-  doctest Content
+  use Plug.Test
+  # @opts AuthPlug.init(%{auth_url: "https://dwylauth.herokuapp.com"})
+
+
+  def dummy(conn, _params) do
+    IO.inspect(conn.halted, label: "conn.halted")
+  end
+
+
+  test "Plug init function doesn't change params" do
+    assert Content.init(%{}) == %{}
+    IO.inspect("hello")
+  end
+
+
+  test "invoke call/2 with accept=json" do
+    conn =
+      conn(:get, "/admin")
+      |> put_req_header("accept", "html")
+      |> Content.call(%{ plugs: [&dummy/2] })
+
+    # IO.inspect(conn, label: "conn:21")
+    # redirect when auth fails
+    # assert conn.status == 302
+  end
 
   test "greets the world" do
     assert Content.hello() == :world
